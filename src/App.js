3cce78eb3a0b3fb8716b40e028cbda55fed91c7e
leonid-06 <harinlenya1488@gmail.com>
2022-10-29 12:37:16 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from 'react'
+import { Route, Routes, Link } from 'react-router-dom'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-
+    <div>
+      <heade>
+        <Link to='/'>Home</Link>
+        <Link to='/abc'>Home</Link>
+        <Link to='/qwe'>Home</Link>
+      </heade>
+      <Routes>
+        <Route path='/' element={<h1>GGGGG</h1>} />
+        <Route path='/abc' element={<h1>abc</h1>} />
+        <Route path='/qwe' element={<h1>qwe</h1>} />
+        <Route path='*' element={<h1>nothing</h1>} />
+      </Routes>
     </div>
-  );
+  )
 }
-
-export default App;
